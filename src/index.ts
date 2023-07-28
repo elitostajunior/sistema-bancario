a0b98a8c933b224e49b9fe203687bb38a33f4051
eligtj <eli_gtj@hotmail.com>
2023-07-28 20:59:49 -0300

    feito o escopo antes de fazer os endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import { accounts } from "./accounts";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.post("/users/create", (req: Request, res: Response) => {
+    try {
+        // validar as entradas da requisição
+        // consultar ou alterar a base de dados
+        const { name, CPF, dateOfBirthAsString } = req.body
+
+        const { day, month, year } = dateOfBirthAsString.split("/")
+        // vai transformar o DD/MM/AAAA em { DD, MM, AAAA } como string
+        
+        const dateOfBirth: Date = new Date(`${year}-${month}-${day}`)
+
+        accounts.push({
+            name,
+            CPF,
+            dateOfBirth,
+            balance: 0,
+            statement: []
+        })
+
+        // validar os resultados da consulta
+        // enviar a resposta
+        res.status(201).send("Conta criada com sucesso!")
+
+    } catch (error: any) {
+        console.log(error)
+        res.status(400).send(error.message)
+    }
+})
 
 app.listen(3003, () => {
     console.log("Servidor rodando na porta 3003")
